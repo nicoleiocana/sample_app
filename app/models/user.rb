44475f11db08d6cb65af5ea01d87003c40064ec9
Nicolei Ocana <nicoleiocana@gmail.com>
2019-04-21 17:49:33 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 5 }
   
   has_secure_password              
 end
