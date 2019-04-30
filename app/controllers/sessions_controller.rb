729cab7121f7b6ef6a56a816c443d3a6d2f54ca1
Nicolei Ocana <nicoleiocana@gmail.com>
2019-04-30 21:35:18 +0000

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,17 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      flash[:success] = "Log in successful"
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        flash[:success] = "Log in successful"
+        redirect_back_or @user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:alert] = message
+        render root_path
+      end
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: [:destroy]
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def new
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_path and return unless @user.activated?
   end
   
   def edit
@@ -21,9 +22,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_path
     else
       render 'new'
     end
